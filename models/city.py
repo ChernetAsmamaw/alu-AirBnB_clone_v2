f3550389d7740720e38f322d9e825fc4495f9bde
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-08 17:59:31 +0300

    updates related to database storage states and classes

--- models/city.py
+++ models/city.py
@@ -5,9 +5,9 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 
 
-class City(BaseModel):
-    """ The city class, contains state ID and name"""
-    __tablename__ = 'cities'
+class City(BaseModel, Base):
+    """ The city class, contains state ID and name """
+    __tablename__ = "cities"
     name = Column(String(128), nullable=False)
-    places = relationship("Place", backref="cities", cascade="delete")
-    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities', cascade="delete")
+    state_id = Column(String(60), ForeignKey("states.id"), nullable=False)
