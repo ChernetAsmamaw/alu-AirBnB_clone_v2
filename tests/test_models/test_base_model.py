8bfed734f95e400ad73cb3796dd897433fa6272c
Pamela UMWALI <p.umwali@alustudent.com>
2023-06-09 13:52:36 +0200

    line 82

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -79,7 +79,7 @@ class test_basemodel(unittest.TestCase):
         n = {'Name': 'test'}
         
         with self.assertRaises(KeyError):
-          new = self.value(**n)
+            new = self.value(**n)
 
 
     def test_id(self):
