13137682080c96f2f47fcc85539c1fa5e325cd94
Pamela UMWALI <p.umwali@alustudent.com>
2023-06-09 13:16:34 +0200

    updated the definition

--- models/base_model.py
+++ models/base_model.py
@@ -17,6 +17,11 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
+        for key, value in kwargs.items():
+            if key not in self.__dict__:
+                raise KeyError(f"Unexpected key '{key}'")
+            setattr(self, key, value)
+
         if not kwargs:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
@@ -67,3 +72,4 @@ class BaseModel:
         """ Deletes the current instance from storage using models.storage """
         from models import storage
         storage.delete(self)
+
