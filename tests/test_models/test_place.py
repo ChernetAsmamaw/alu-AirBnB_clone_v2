e5758a280563c98b22132447435274a4392d1ffe
Pamela UMWALI <p.umwali@alustudent.com>
2023-06-09 19:23:07 +0200

    place

--- tests/test_models/test_place.py
+++ tests/test_models/test_place.py
@@ -2,11 +2,13 @@
 """ """
 from tests.test_models.test_base_model import test_basemodel
 from models.place import Place
-from sqlalchemy import Column, String
+from models.city import City
+from models.user import User
+
 
 
 class test_Place(test_basemodel):
-    """ """
+    """ test for place """
     def __init__(self, *args, **kwargs):
         """ """
         super().__init__(*args, **kwargs)
@@ -16,46 +18,61 @@ class test_Place(test_basemodel):
     def test_city_id(self):
         """ """
         new = self.value()
+        city = City()
         self.assertEqual(type(new.city_id), str)
+
+    def test_user_id(self):
+        """ """
+        new = self.value()
+        user = User()
+        self.assertEqual(type(new.user_id), str)
 
     def test_name(self):
         """ """
         new = self.value()
+        new.name = "Place"
         self.assertEqual(type(new.name), str)
 
     def test_description(self):
         """ """
         new = self.value()
+        new.description = ""
         self.assertEqual(type(new.description), str)
 
     def test_number_rooms(self):
         """ """
         new = self.value()
+        new.number_rooms = 4
         self.assertEqual(type(new.number_rooms), int)
 
     def test_number_bathrooms(self):
         """ """
         new = self.value()
+        new.number_bathrooms = 2
         self.assertEqual(type(new.number_bathrooms), int)
 
     def test_max_guest(self):
         """ """
         new = self.value()
+        new.max_guest = 8
         self.assertEqual(type(new.max_guest), int)
 
     def test_price_by_night(self):
         """ """
         new = self.value()
+        new.price_by_night = 20
         self.assertEqual(type(new.price_by_night), int)
 
     def test_latitude(self):
         """ """
         new = self.value()
+        new.latitude = -1.23
         self.assertEqual(type(new.latitude), float)
 
     def test_longitude(self):
         """ """
         new = self.value()
+        new.longitude = -1.32
         self.assertEqual(type(new.latitude), float)
 
     def test_amenity_ids(self):
