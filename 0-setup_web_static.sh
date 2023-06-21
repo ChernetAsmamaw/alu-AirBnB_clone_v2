8f68696c6a9bb4784a0ba664e121b45e3e87ed94
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-21 15:42:52 +0300

    prepare web servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file with sample to test Nginx configuration
-sudo echo "<html><head></head><body><h1>Test Nginx Configuration</h1></body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html><head></head><body>Test Nginx configuration</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link. If the symbolic link already exists, it is deleted and recreated every time the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -21,5 +21,4 @@ sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 # Restart nginx after updating the configuration.
-
 sudo service nginx restart
