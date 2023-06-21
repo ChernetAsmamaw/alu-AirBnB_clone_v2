7c2145327362306400d2864c9712d482f2338257
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-21 15:51:32 +0300

    prepare web servers for deployment

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,21 @@ sudo apt-get -y install nginx
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file with sample to test Nginx configuration
-sudo echo "<html><head></head><body>Test Nginx configuration</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>
+ubuntu@5419-web-01:~/$ curl localhost/hbnb_static/index.html
+<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
 # Create a symbolic link. If the symbolic link already exists, it is deleted and recreated every time the script is ran.
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
