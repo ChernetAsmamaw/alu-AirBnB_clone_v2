bc9b04e0134f58fcac9547e85190e901d559c85f
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-22 11:36:44 +0300

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy:

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -12,21 +12,22 @@ def do_deploy(archive_path):
     """Function to distribute an archive to your web servers"""
     if not exists(archive_path):
         return False
+    file_name = archive_path.split("/")[-1]
+    file_wout = file_name.split(".")[0]
+    path = "/data/web_static/releases/"
     try:
-        file_n = archive_path.split("/")[-1]
-        no_ext = file_n.split(".")[0]
-        path = "/data/web_static/releases/"
         put(archive_path, '/tmp/')
-        run('mkdir -p {}{}/'.format(path, no_ext))
-        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
-        run('rm /tmp/{}'.format(file_n))
-        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
-        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('mkdir -p {}{}/'.format(path, file_wout))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_name, path, file_wout))
+        run('rm /tmp/{}'.format(file_name))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, file_wout))
+        run('rm -rf {}{}/web_static'.format(path, file_wout))
         run('rm -rf /data/web_static/current')
-        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        run('ln -s {}{}/ /data/web_static/current'.format(path, file_wout))
         return True
     except Exception:
         return False
+
 
 # Run the script like this:
 # $ fab -f 2-do_deploy_web_static.py
