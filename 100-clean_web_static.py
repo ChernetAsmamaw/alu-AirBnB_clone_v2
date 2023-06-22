06db3dc96327629face2a57c296e200d7a51d67d
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-22 12:17:46 +0300

    Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean:

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,1 +1,31 @@
 #!/usr/bin/python3
+"""FabScript that deletes out-of-date archives"""
+
+import os
+from fabric.api import *
+
+env.hosts = ["3.90.35.215", "34.226.202.215"]
+
+
+def do_clean(number=0):
+    """
+    Deletes out-of-date archives.
+    number: number of archives to keep
+    """
+    number = 1 if int(number) == 0 else int(number)
+
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(archive)) for archive in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(archive)) for archive in archives]
+
+
+# Run the script like this:
+# First "ls -ltr versions" | to see the list of archives
+# >>> $ fab -f 100-clean_web_static.py do_clean:number=2
+# -i my_ssh_private_key -u ubuntu > /dev/null 2>&1
