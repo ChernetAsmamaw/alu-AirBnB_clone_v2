662725afb7313447800cdaf162ab9e12f7b1625a
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-24 15:55:56 +0300

    updates to fab files

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,24 +1,23 @@
 #!/usr/bin/python3
-"""Fabfile to generate a .tgz archive from the contents
-of the web_static folder of the AirBnB Clone repo through
-the function do_pack"""
+"""Fabric script which generates a tgz archive"""
 
-from fabric.api import local
 from datetime import datetime
-import os
+from fabric.api import local
+from os.path import isdir
 
 
 def do_pack():
-    """Generates a compressed archive of the web_static dir"""
-    time_now = datetime.now().strftime("%Y%m%d%H%M%S")
-    local("mkdir -p versions")
-    archive_name = "versions/web_static_{}.tgz".format(time_now)
-    result = local("tar -cvzf {} web_static".format(archive_name))
-
-    if result.succeeded:
-        return archive_name
-    else:
+    """Fabric script which generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
         return None
+
 
 # This file should be run using this syntax:
 # fab -f 1-pack_web_static.py do_pack
