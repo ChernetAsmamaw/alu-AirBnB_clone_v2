608f4e1e47f012c7a03f65de5773a446b6ea09fb
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-24 15:37:11 +0300

    updates for a new server

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 # Write a Bash script that sets up your web servers for the deployment of web_static.
 
+
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo service nginx start
 
 # Create the following folders if they don’t already exist:
 sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
 
 # Create a fake HTML file with sample to test Nginx configuration
-echo "<html><head></head><body>Test Nginx configuration</body></html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Hello, this is a test HTML file." | sudo tee /data/web_static/releases/test/index.html
+
+sudo rm -rf /data/web_static/current
 
 # Create a symbolic link. If the symbolic link already exists, it is deleted and recreated every time the script is ran.
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of the /data/ folder to the ubuntu user AND group. This should be recursive.
 sudo chown -R ubuntu:ubuntu /data/
