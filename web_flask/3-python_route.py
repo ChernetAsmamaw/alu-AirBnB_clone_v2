32c9eec83a143c622d92668a5ea567b373c3f32d
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-27 18:51:31 +0300

    scripts that start a flask web app

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""
-A python script that starts a Flask web application
-"""
+"""A script that starts a Flask web application"""
 
 from flask import Flask
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello():
-    """Display Hello HBNB! from the root route /"""
+def hello_hbnb():
+    """A function that displays Hello HBNB!"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Display HBNB from the route /hbnb"""
+    """A function that displays HBNB"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def c(text):
-    """Display C {...} from the route /c/<text>"""
+def c_text(text):
+    """A function that displays C followed by text"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python/', defaults={'text': 'is cool'} strict_slashes=False)
-@app.route('/python/<text>', strict_slashes=False)
-def python(text):
-    """Display Python {...} from the route /python/<text>"""
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
+def python_text(text):
+    """A function that displays Python followed by text"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
