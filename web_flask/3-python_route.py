b811edb3ec81916082013f1091e0cb047f2c65b8
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-27 18:55:19 +0300

    scripts that start a flask web app

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -7,26 +7,26 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
-    """A function that displays Hello HBNB!"""
+    """Displays 'Hello HBNB!' in the route /"""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """A function that displays HBNB"""
+    """Displays 'HBNB' in the route /hbnb"""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
-    """A function that displays C followed by text"""
+    """Displays 'C' followed by the value of the text variable"""
     return 'C {}'.format(text.replace('_', ' '))
 
 
-@app.route('/python', strict_slashes=False)
-@app.route('/python/<text>', defaults={'text': 'is cool'}, strict_slashes=False)
-def python_text(text):
-    """A function that displays Python followed by text"""
+@app.route('/python', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text):
+    """Displays 'Python' followed by the value of the text variable"""
     return 'Python {}'.format(text.replace('_', ' '))
 
 
