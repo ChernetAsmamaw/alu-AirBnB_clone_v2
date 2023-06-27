4d4823eb56b0644befb8c6cfffd841dde9b67dd3
root <c.asmamaw@alustudnet.com>
2023-06-27 15:43:22 +0000

    scripts that start a Flask web app

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
-"""A python script that starts a Flask web application"""
+"""
+A python script that starts a Flask web application
+"""
 
 from flask import Flask
 app = Flask(__name__)
