691432dc65665d3cc253f1197ca072f4919d3a42
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-29 16:33:09 +0300

    scripts that start a flask web app

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -10,15 +10,14 @@ app = Flask(__name__)
 
 
 @app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
+@app.route('/states/<state_id>', strict_slashes=False)
 def states(state_id=None):
     """Display a HTML page: (inside the tag BODY)"""
     # H1 tag: "States" inside the tag BODY
     # UL tag: with the list of all State objects present in DBStorage
     # LI tag: description of one State: <state.id>: <B><state.name></B>
     # LI tag: description of one City: <city.id>: <B><city.name></B>
-    # sorted by name (A->Z)
-    states = storage.all("State")
+    # sorted by name (A->Z)    states = storage.all("State")
     if state_id is not None:
         state_id = 'State.' + state_id
     return render_template('9-states.html', states=states, state_id=state_id)
@@ -31,4 +30,4 @@ def teardown_db(exception):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port='5000')
