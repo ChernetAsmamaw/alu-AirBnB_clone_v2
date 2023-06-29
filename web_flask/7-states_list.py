7c6a23996e3fae3942b0b0cbdd6fd39910fce538
ChernetAsmamaw <c.asmamaw@alustudent.com>
2023-06-29 15:28:17 +0300

    scripts that start a flask web app

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """A python script that starts a Flask web application"""
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 from models import storage
 from models.state import State
-from flask import Flask, render_template, request, jsonify
 from os import getenv
 
 app = Flask(__name__)
